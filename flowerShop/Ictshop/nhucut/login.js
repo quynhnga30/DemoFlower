daae4caf4b9b3fbfb790ac89ead66faad5cbaa7a
quynhnga30 <93904468+quynhnga30@users.noreply.github.com>
2023-04-01 10:53:10 +0700

    add
    add

---
+++ flowerShop/Ictshop/nhucut/login.js
@@ -0,0 +1,10 @@
+﻿let containers = document.getElementById('containers')
+
+toggle = () => {
+	containers.classList.toggle('sign-in')
+	containers.classList.toggle('sign-up')
+}
+
+setTimeout(() => {
+	containers.classList.add('sign-in')
+}, 200)
